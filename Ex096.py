229cb66d9c832065d3a092cf8f9bf18a75686ef2
Syderon O. Silva <138392147+Syderon@users.noreply.github.com>
2023-09-17 21:53:44 -0300

    Add files via upload

---
+++ Ex096.py
@@ -0,0 +1,11 @@
+print("-"*30)
+print("CONTROLE DE TERRENO")
+print("-"*30)
+def area(x, y):
+   a = x*y
+   print(f"A aréa de um terreno de {x}x{y} e de {a}m² ")
+
+x = float(input("LARGURA (m): "))
+y = float(input("COMPRIMENTO (m): "))
+area(x, y)
+
---
+++ Ex097.py
@@ -0,0 +1,9 @@
+def nome(txt):
+    print(txt)
+    print('─'*len(txt)) #definindo o tamanho da linha de acordo com cada  FRASE!
+
+
+nome('Python é foda!')
+nome('Python é melhor qaue JAVA')
+nome('Python')
+
